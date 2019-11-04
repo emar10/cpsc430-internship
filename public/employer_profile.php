6b2422a0f73360606889f2009edcf75d9640c055
mwindsor74 <33789255+mwindsor74@users.noreply.github.com>
2019-11-04 10:39:58 -0500

    Update and rename employer_details.php to employer_profile.php

--- public/employer_profile.php
+++ public/employer_profile.php
@@ -9,8 +9,8 @@
 	<body>
 		<div class="nav">
 			<ul>
-  				<li><a href="internship_home.html">Internships Home</a></li>
-  				<li><a href="add_review.html">Submit an Internship Review</a></li>
+  				<li><a href="internship_home.php">Internships Home</a></li>
+  				<li><a href="add_review.php">Submit an Internship Review</a></li>
 			</ul> 
 		</div>
 		<div class="employerProfile">
@@ -48,7 +48,7 @@
 				<p>SimVentions was a pleasure to work for, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<hr>
 				<center>
-					<form action="add_review.html" style="margin-top: 20px; margin-bottom: 20px;">
+					<form action="add_review.php" style="margin-top: 20px; margin-bottom: 20px;">
 							<input type="submit" value="Submit a Review" class="submit" />
 					</form>
 				</center>
